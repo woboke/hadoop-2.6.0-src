337055d17e167fb12794e6e34928c3b0712198f4
JLS <jls@JLSdeMacBook-Pro.local>
2017-02-07 18:52:58 +0800

    Initial commit

---
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/protocolPB/test_chenhong.java
@@ -0,0 +1,12 @@
+package org.apache.hadoop.hdfs.protocolPB;
+
+/**
+ * Created by jls on 2016/12/10.
+ */
+public class test_chenhong {
+ public static void main(String args[]){
+  System.out.println("you are the one");
+  System.out.println();
+ }
+
+}
